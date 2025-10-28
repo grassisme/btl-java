ecd7d005aed22f4384ec9f29aef4f6bb7a8a8788
AbrahamZenon <zenon3k@gmail.com>
2025-10-29 05:36:06 +0700

    Add files via upload

---
+++ Jchess/src/com/chess/engine/pieces/Bishop.java
@@ -0,0 +1,40 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.List;
+
+public final class Bishop extends Piece {
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -7, 7, 9};
+    private final static Map<Integer, MoveUtils.Line[]> PRECOMPUTED_CANDIDATES = computeCandidates();
+
+    Bishop(final Alliance alliance,
+           final int piecePosition,
+           final boolean isFirstMove) {
+        super(PieceType.BISHOP, alliance, piecePosition, isFirstMove);
+    }
+    
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+        for (final MoveUtils.Line line : PRECOMPUTED_CANDIDATES.get(this.piecePosition)) {
+            for (final int candidateDestinationCoordinate : line.getLineCoordinates()) {
+                final Piece pieceAtDestination = board.getPiece(candidateDestinationCoordinate);
+                if (pieceAtDestination == null) {
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                } else {
+                    final Alliance pieceAlliance = pieceAtDestination.getPieceAllegiance();
+                    if (this.pieceAlliance != pieceAlliance) {
+                        legalMoves.add(new MajorAttackMove(board, this, candidateDestinationCoordinate,
+                                pieceAtDestination));
+                    }
+                    break;
+                }
+            }
+        }
+        return Collections.unmodifiableList(legalMoves);
+    }
+
+}
