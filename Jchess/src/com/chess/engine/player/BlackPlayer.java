faec7ca832448f6d1e8e343f850806dd684f4374
NT Dung <nguyentiendung.190905@gmail.com>
2025-11-29 17:46:06 +0700

    update player

---
+++ Jchess/src/com/chess/engine/player/BlackPlayer.java
@@ -0,0 +1,12 @@
+package com.chess.engine.player;
+
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.King;
+
+import java.util.Collection;
+
+public class BlackPlayer extends Player{
+    public BlackPlayer(Board board, King king, Collection<Move> whiteStandardMoves, Collection<Move> blackStandardMoves) {
+    }
+}
---
+++ Jchess/src/com/chess/engine/player/WhitePlayer.java
@@ -0,0 +1,12 @@
+package com.chess.engine.player;
+
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.King;
+
+import java.util.Collection;
+
+public class WhitePlayer {
+    public WhitePlayer(Board board, King king, Collection<Move> whiteStandardMoves, Collection<Move> blackStandardMoves) {
+    }
+}
